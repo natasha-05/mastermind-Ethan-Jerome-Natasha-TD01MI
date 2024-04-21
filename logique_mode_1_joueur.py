8b6e9aa41bdb2110fbb1f799edaa6be9ece8b6dc
natasha-05 <145109339+natasha-05@users.noreply.github.com>
2024-04-21 19:41:49 +0200

    Add files via upload

--- logique_mode_1_joueur.py
+++ logique_mode_1_joueur.py
@@ -1,6 +1,7 @@
- from random import *
 
-choix_couleur = ["white", "blue", "green", "yellow", "pink", "purple", "orange", "black"] 
+from random import *
+
+choix_couleur = ["white", "blue", "green", "yellow", "pink", "purple", "orange", "black", "v", "w", "x"] 
 code_secret = []
 
 for i in range(4):
@@ -12,54 +13,58 @@ print("Code secret :XXXX" )
 
 
 #joueur 2 décode
-proposition = []
 
-for i in range(4):
+# Boucle pour les tentatives du joueur 2
+for tentative in range(1, 11):
+    print("\nTentative", tentative)
+    proposition = []#initialise les proposition pour chaque tentative
 
-    J2 = (input("joueur2 décodez  : "))
-    proposition.append(J2)
+    for i in range(4):
+
+        J2 = (input("joueur2 décodez  : "))
+        proposition.append(J2)
+        
+
+    #INDICE:    
     
 
-#INDICE:    
+    #Compte le nombre de lettres étrangères dans une proposition par rapport au code secret.
+    def couleur_etrangees(code_secret, proposition):
+        
+        couleur_etrangees = 0
+        for lettre in proposition:
+            if lettre not in code_secret:
+                couleur_etrangees += 1
+        return couleur_etrangees
+
+
+    def couleur_mal_placees(code_secret, proposition):
+        mal_placees = 0
+        couleur_utilisees = []  # Liste pour stocker les couleurs déjà vérifiées
+        for i in range(len(code_secret)):
+            if proposition[i] != code_secret[i] and proposition[i] in code_secret and proposition[i] not in couleur_utilisees:
+                mal_placees += 1
+                couleur_utilisees.append(proposition[i])  # Ajouter la couleur vérifiée à la liste
+        return mal_placees
+
+
+
+
+    def compare(code_secret, proposition):
+        couleurs_bien_placees = 0
+        for i in range(len(code_secret)):
+            if code_secret[i] == proposition[i]:
+                couleurs_bien_placees += 1  
+        print(couleurs_bien_placees, "couleurs bien placées.")
+        
+        mal_placees = couleur_mal_placees(code_secret, proposition)
     
+        print(mal_placees, "couleurs mal placées.")
 
-#Compte le nombre de lettres étrangères dans une proposition par rapport au code secret.
-def couleur_etrangees(code_secret, proposition):
-    
-    couleur_etrangees = 0
-    for lettre in proposition:
-        if lettre not in code_secret:
-            couleur_etrangees += 1
-    return couleur_etrangees
+    compare(code_secret, proposition)
 
-
-def couleur_mal_placees(code_secret, proposition):
-    mal_placees = 0
-    couleur_utilisees = []  # Liste pour stocker les couleurs déjà vérifiées
-    for i in range(len(code_secret)):
-        if proposition[i] != code_secret[i] and proposition[i] in code_secret and proposition[i] not in couleur_utilisees:
-            mal_placees += 1
-            couleur_utilisees.append(proposition[i])  # Ajouter la couleur vérifiée à la liste
-    return mal_placees
-
-
-
-
-def compare(code_secret, proposition):
-    couleurs_bien_placees = 0
-    for i in range(len(code_secret)):
-        if code_secret[i] == proposition[i]:
-            couleurs_bien_placees += 1  
-    print(couleurs_bien_placees, "couleurs bien placées.")
-    
-    mal_placees = couleur_mal_placees(code_secret, proposition)
-   
-    print(mal_placees, "couleurs mal placées.")
-
-compare(code_secret, proposition)
-
-if proposition == code_secret:
-     print("BRAVO!!! vous avez décodé")
+    if proposition == code_secret:
+        print("BRAVO!!! vous avez décodé")
 else:
      print("Vous avez perdu.")            
 
--- logique_mode_2_joueurs.py
+++ logique_mode_2_joueurs.py
@@ -1,54 +1,58 @@
-
- #le joueur1 forme son code secret
-                 
-choix_couleur = ["white", "blue", "green", "yellow", "pink", "purple", "orange", "black", "v","w","x"] 
+# Le joueur1 forme son code secret
+choix_couleur = ["white", "blue", "green", "yellow", "pink", "purple", "orange", "black"] 
 code_secret = []
 
-
 for i in range(4):
- 
-    J1 = (input("joueur1 Entrez le code secret : "))
-    if J1 in choix_couleur:
-        code_secret.append(J1)
-    else:
-        print("Couleur invalide. Veuillez entrer une couleur dans la palette proposée.")
-
-print("Code secret :", code_secret)  # par la suite Affiche une chaîne de caractères à la place du code secret avec XXXX
-
-#joueur 2 décode
-decode = []
-
-for i in range(4):
- 
-    J2 = (input("joueur2 décodez  : "))
-    if J2  in choix_couleur:
-        decode.append(J2)
-    else:
-        print("Couleur invalide. Veuillez entrer une couleur dans la palette proposée.")
-
-#INDICE:    
+    J1 = input("Joueur 1, entrez le code secret : ")
+    code_secret.append(J1)
     
-#verifie si les couleurs sont bien placé et fait la somme
-def compare(code_secret,decode):
+print("Code secret :", code_secret)  
 
-    couleurs_bien_placees = 0
-   
+# Boucle pour les tentatives du joueur 2
+for tentative in range(1, 11):
+    print("\nTentative", tentative)
+    proposition = []  # Initialise la proposition pour chaque tentative
 
-    for i in range(len(code_secret)):
-        if code_secret[i] == decode[i]:
-            couleurs_bien_placees += 1  
-    print(couleurs_bien_placees, "couleurs bien placées.")
-        
-# somme des couleur mal placé
-     
-    couleur_mal_place = 0
-    
-    
-    for i, element in enumerate(code_secret):
-            if decode[i] != element :
-                couleur_mal_place += 1
+    # Joueur 2 décode
+    for i in range(4):
+        J2 = input("Joueur 2, décodez : ")
+        proposition.append(J2)
 
-    print(couleur_mal_place," couleur mal placé ")
-            
-        
-compare(code_secret,decode)
+    # Fonction pour compter le nombre de lettres étrangères dans la proposition
+    def couleur_etrangees(code_secret, proposition):
+        couleur_etrangees = 0
+        for lettre in proposition:
+            if lettre not in code_secret:
+                couleur_etrangees += 1
+        return couleur_etrangees
+
+    # Fonction pour compter le nombre de couleurs mal placées dans la proposition
+    def couleur_mal_placees(code_secret, proposition):
+        mal_placees = 0
+        couleur_utilisees = []  # Liste pour stocker les couleurs déjà vérifiées
+        for i in range(len(code_secret)):
+            if proposition[i] != code_secret[i] and proposition[i] in code_secret and proposition[i] not in couleur_utilisees:
+                mal_placees += 1
+                couleur_utilisees.append(proposition[i])  # Ajouter la couleur vérifiée à la liste
+        return mal_placees
+
+    # Fonction pour comparer la proposition avec le code secret
+    def compare(code_secret, proposition):
+        couleurs_bien_placees = 0
+        for i in range(len(code_secret)):
+            if code_secret[i] == proposition[i]:  # Si la couleur est bien placée
+                couleurs_bien_placees += 1  # Incrémente le nombre de couleurs bien placées
+        print(couleurs_bien_placees, "couleurs bien placées.")
+
+        mal_placees = couleur_mal_placees(code_secret, proposition)  # Appel de la fonction pour compter les couleurs mal placées
+        print(mal_placees, "couleurs mal placées.")
+
+    # Appel de la fonction pour comparer la proposition avec le code secret
+    compare(code_secret, proposition)
+
+    # Vérification si la proposition est égale au code secret
+    if proposition == code_secret:
+        print("BRAVO !!! Vous avez décodé.")
+        break
+else:
+    print("Vous avez utilisé toutes vos tentatives. Le code secret était :", code_secret)
